cae80471e605eed2cc3936adaa2126faf08a1892
Trippixyz <felix.edyou@gmail.com>
2024-01-19 22:42:17 +0100

    Remove leftover practice stuff

--- src/program/main.cpp
+++ src/program/main.cpp
@@ -5,34 +5,17 @@
 #include "heap/seadExpHeap.h"
 #include "heap/seadHeapMgr.h"
 #include "hook/trampoline.hpp"
+#include "imgui.h"
 #include "lib.hpp"
 #include "nn/fs.h"
 #include "program/imgui_nvn.h"
 #include <sead/filedevice/seadFileDeviceMgr.h>
 
-HOOK_DEFINE_TRAMPOLINE(FileDeviceMgrCtor) { static void Callback(sead::FileDeviceMgr * thisPtr); };
-void FileDeviceMgrCtor::Callback(sead::FileDeviceMgr* thisPtr)
-{
-    Orig(thisPtr);
-    thisPtr->mMountedSd = nn::fs::MountSdCardForDebug("sd").IsSuccess();
-}
-
-class HakoniwaSequence;
-HOOK_DEFINE_TRAMPOLINE(HakoniwaSequenceInit) { static void Callback(HakoniwaSequence * thisPtr, const al::SequenceInitInfo& info); };
-void HakoniwaSequenceInit::Callback(HakoniwaSequence* thisPtr, const al::SequenceInitInfo& info)
-{
-    Orig(thisPtr, info);
-}
-
-HOOK_DEFINE_TRAMPOLINE(HakoniwaSequenceUpdate) { static void Callback(HakoniwaSequence * thisPtr); };
-void HakoniwaSequenceUpdate::Callback(HakoniwaSequence* thisPtr)
-{
-    Orig(thisPtr);
-}
-
 static void drawDbgGui()
 {
-    
+    ImGui::GetIO().FontGlobalScale = .5;
+
+    ImGui::Text("Hello World!");
 }
 
 HOOK_DEFINE_REPLACE(DisableATrigger) {
@@ -44,15 +27,6 @@ HOOK_DEFINE_REPLACE(DisableATrigger) {
 extern "C" void exl_main(void* x0, void* x1)
 {
     exl::hook::Initialize();
-
-    using Patcher = exl::patch::CodePatcher;
-    using namespace exl::patch::inst;
-
-    //FileDeviceMgrCtor::InstallAtOffset(pe::offsets::FileDeviceMgrCtorHookLocation);
-    //HakoniwaSequenceInit::InstallAtOffset(pe::offsets::HakoniwaSequenceInitHookLocation);
-    //HakoniwaSequenceUpdate::InstallAtOffset(pe::offsets::HakoniwaSequenceUpdate);
-
-    DisableATrigger::InstallAtFuncPtr(al::isPadTriggerA);
 
     nvnImGui::InstallHooks();
     nvnImGui::addDrawFunc(drawDbgGui);
